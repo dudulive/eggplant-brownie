8edd0550e247bedb3ab5f717c8f8e865cb15edd6
edu <edu@edus-Mac.local>
2019-01-11 17:42:57 -0800

    iniciando o projeto

---
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by edu on 11/01/19.
+//  Copyright © 2019 edu braz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
